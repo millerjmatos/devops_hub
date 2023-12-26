0f7da0a6f0328ad06d10736bfed474506f1d43fc
Muller Jorge <millerjmatos@hotmail.com>
2023-12-26 20:46:56 -0300

    feat: sort update

---
+++ python/projects/sorteios/6n_noseq.py
@@ -0,0 +1,34 @@
+import random
+
+def tem_consecutivos(nums):
+    return any(nums[i] + 1 == nums[i + 1] for i in range(len(nums) - 1))
+
+def gerar_sorteio():
+    while True:
+        numeros_disponiveis = [num for num in range(1, 61) if num not in [2, 9, 18, 19, 26, 28, 44, 45, 48, 50]]
+        
+        impares = [num for num in numeros_disponiveis if num % 2 != 0]
+        pares = [num for num in numeros_disponiveis if num % 2 == 0]
+
+        num_imp = random.sample(impares, 3)
+        num_par = random.sample(pares, 3)
+
+        # Junte todos os números escolhidos, ordene-os e verifique se há consecutivos
+        numeros_sorteados = sorted(num_imp + num_par)
+        if not tem_consecutivos(numeros_sorteados):
+            break
+
+    return numeros_sorteados
+
+# Exemplo de uso
+resultado_sorteio = gerar_sorteio()
+print("Números Sorteados:", resultado_sorteio)
+
+# Adicionar os números sorteados ao arquivo sem excluir o conteúdo existente
+with open('resultados.txt', 'a') as arquivo:
+    for i, numero in enumerate(resultado_sorteio, start=1):
+        arquivo.write(str(numero))
+        if i % 6 == 0:
+            arquivo.write('\n')
+        else:
+            arquivo.write(' ')
